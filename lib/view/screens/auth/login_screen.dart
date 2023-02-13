bef01c3423960523fa661b5da60de847372be986
Shajedul Islam Pritom <shajedul35-569@diu.edu.bd>
2023-02-13 22:58:47 +0600

    minos changes

--- lib/view/screens/auth/login_screen.dart
+++ lib/view/screens/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:messenger/constants/constants.dart';
 import 'package:get/get.dart';
 import 'package:messenger/controller/auth_controller.dart';
@@ -14,104 +15,129 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.all(12),
-          child: Center(
-            child: Form(
-              key: _formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Text('Login Now, অনেক কথা জমে আছে!', style: titleTextStyle,),
-                    SizedBox(
-                      height: 200,
-                      child: Image.asset('assets/images/banner.png'),
-                    ),
-                    verticalSpace(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: 50,
-                          child: Image.asset('assets/images/logo.png'),
-                        ),
-                        horizontalSpace(),
-                        Text('KOTHA BOLUN', style: titleTextStyle),
-                      ],
-                    ),
-                    verticalSpace(),
-                    TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'This field can\'t be empty';
-                        } else if (!value.contains(RegExp('@'))) {
-                          return 'Enter a valid email';
-                        } else {
-                          return null;
-                        }
-                      },
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.email),
-                        labelText: 'Email',
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: borderColor,
-                            ),
-                            borderRadius: BorderRadius.circular(10)),
+      body: WillPopScope(
+        onWillPop: (){
+          return alertDialog(
+              context: context,
+              title: 'Are you sure to exit?',
+              action: [
+                TextButton(
+                    onPressed: () {
+                      SystemNavigator.pop();
+                    },
+                    child: Text(
+                      'Yes',
+                      style: blueText,
+                    )),
+                TextButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: Text(
+                      'No',
+                      style: blueText,
+                    )),
+              ]);
+        },
+        child: SafeArea(
+          child: Container(
+            margin: EdgeInsets.all(12),
+            child: Center(
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text('Login Now, অনেক কথা জমে আছে!', style: titleTextStyle,),
+                      SizedBox(
+                        height: 200,
+                        child: Image.asset('assets/images/banner.png'),
                       ),
-                    ),
-                    verticalSpace(),
-                    TextFormField(
-                      obscureText: true,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'This field can\'t be empty';
-                        } else {
-                          return null;
-                        }
-                      },
-                      controller: _passController,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        labelText: 'Password',
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: borderColor,
-                            ),
-                            borderRadius: BorderRadius.circular(10)),
+                      verticalSpace(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 50,
+                            child: Image.asset('assets/images/logo.png'),
+                          ),
+                          horizontalSpace(),
+                          Text('KOTHA BOLUN', style: titleTextStyle),
+                        ],
                       ),
-                    ),
-                    verticalSpace(),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2,
-                      child: customTextButton(
-                        text: 'Login',
-                        bgColor: Colors.blue,
-                        onTap: () {
-                          if (_formKey.currentState!.validate()) {
-                            AuthContoller.instance.login(
-                                _emailController.text, _passController.text);
+                      verticalSpace(),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'This field can\'t be empty';
+                          } else if (!value.contains(RegExp('@'))) {
+                            return 'Enter a valid email';
+                          } else {
+                            return null;
                           }
                         },
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.email),
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: borderColor,
+                              ),
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
                       ),
-                    ),
-                    verticalSpace(),
-                    Row(
-                      children: [
-                        Text('Don\'t have an account?'),
-                        InkWell(
-                            onTap: () {
-                              Get.toNamed('/register');
-                            },
-                            child: Text(
-                              ' Register Now',
-                              style: TextStyle(color: blueColor),
-                            ))
-                      ],
-                    )
-                  ],
+                      verticalSpace(),
+                      TextFormField(
+                        obscureText: true,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'This field can\'t be empty';
+                          } else {
+                            return null;
+                          }
+                        },
+                        controller: _passController,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: borderColor,
+                              ),
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                      verticalSpace(),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: customTextButton(
+                          text: 'Login',
+                          bgColor: Colors.blue,
+                          onTap: () {
+                            if (_formKey.currentState!.validate()) {
+                              AuthContoller.instance.login(
+                                  _emailController.text, _passController.text);
+                            }
+                          },
+                        ),
+                      ),
+                      verticalSpace(),
+                      Row(
+                        children: [
+                          Text('Don\'t have an account?'),
+                          InkWell(
+                              onTap: () {
+                                Get.toNamed('/register');
+                              },
+                              child: Text(
+                                ' Register Now',
+                                style: TextStyle(color: blueColor),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
